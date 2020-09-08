cb25293edd2ce92a436fc3df3057c9dc5f32967b
Kaylee Cho <kaylee.j.cho@gmail.com>
2020-09-08 16:44:57 +0200

    using form/ data posting works

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -1,9 +1,24 @@
 package controllers
 import javax.inject._
+
 import play.api.mvc._
 
 @Singleton
 class TaskList1 @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def login = Action {
+    Ok(views.html.login1())
+  }
+  def validateLoginGet(username: String, password: String) = Action {
+    Ok(s"$username and $password")
+  }
+  def validateLoginPost = Action { request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      Ok(s"$username looged in with $password")
+    }.getOrElse(Ok("Oops"))
+  }
   def taskList =  Action {
     val tasks = List("sleep", "code", "yoga", "eat", "play with booboo")
     Ok(views.html.taskList1(tasks))
